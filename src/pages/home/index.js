1d33d2eea503fc4893e61e7e39d92cd73365a164
121110669 GABRIEL DANTAS DE OLIVEIRA <gabriel.dantas.oliveira@lcc1-17.lcc.ufcg.edu.br>
2024-02-05 09:16:41 -0300

    Header

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/header';
+
+//IMPORTANDO O CSS
+// import ''
+
+function Home() {
+    return (
+      <section className="App">
+        <Header />
+      </section>
+    );
+  }
+  
+export default Home;
